3c001d927a368beeb36e12057c51fe148527de5a
dusan_mitrovic <dusanmitrovic@elfak.rs>
2020-04-27 23:50:32 +0200

    Pokusaj izmene autora

--- Aplikacija/RIIEdition/Pages/UserPages/ProfilPage.cshtml.cs
+++ Aplikacija/RIIEdition/Pages/UserPages/ProfilPage.cshtml.cs
@@ -11,46 +11,18 @@ namespace RIIEdition.Pages.UserPages
 {
     public class ProfilPageModel : PageModel
     {
-        [BindProperty(SupportsGet=true)]
-        public User MyUser{get;set;}
-
         private readonly UserManager<User> userManager;
 
         public ProfilPageModel(UserManager<User> userManager)
         {
             this.userManager = userManager;
         }
-
+        [BindProperty(SupportsGet=true)]
+        public User MyUser{get;set;}
         public async Task OnGetAsync()
         {
            MyUser=await  userManager.FindByNameAsync(User.Identity.Name);
+            
         }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-          var user = await  userManager.FindByNameAsync(User.Identity.Name);
-          user.UserName = MyUser.UserName; 
-          user.Email = MyUser.Email;
-          user.Name = MyUser.Name;
-          user.LastName = MyUser.LastName;
-          user.Gender = MyUser.Gender;
-          user.YearOfBirth = MyUser.YearOfBirth;
-          user.PhoneNumber = MyUser.PhoneNumber;
-          user.City = MyUser.City;
-          user.Index = MyUser.Index;
-          user.YearOfStudy = MyUser.YearOfStudy;
-         
-          var result=await userManager.UpdateAsync(user);
-          if(result.Succeeded)
-          return RedirectToPage("/UserPages/ProfilPage");
-          else
-          {
-              foreach (var error in result.Errors)
-              {
-                  ModelState.AddModelError("",error.Description);
-              }
-              return Page();
-          }
-        }        
     }
 }
